65cf2f945a46d2d9491826db433f82a630cb10fd
Marshall <marshall.peters01@gmail.com>
2023-04-22 12:55:56 -0400

    added fs, many questions to inquirer and init()

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -14,13 +15,76 @@ const questions = [
       message: 'Please provide a brief description of your project:',
       default: 'This is a project.'
     },
-    // Add more later
+    {
+      type: 'confirm',
+      name: 'tableOfContents',
+      message: 'Will you be needing a table of contents for this README?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please provide installation instructions:',
+      default: 'npm install'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please provide usage information:',
+      default: 'npm start'
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Please provide contribution guidelines:',
+      default: 'Contributions are welcome.'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Please provide test instructions:',
+      default: 'npm test'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Please choose a license:',
+      choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-Clause', 'Unlicensed']
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
+      default: 'username'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+      default: 'email@example.com'
+    },
   ];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then((answers) => {
+      const template = `# ${answers.title}\n\n${answers.description}\n`;
+      writeToFile('README.md', template);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Successfully wrote ${fileName}`);
+    }
+  });
+}
 // Function call to initialize app
 init();
